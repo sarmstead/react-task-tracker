8438c840d0ed43080adea5c39b8388afb924e9ae
sunjay_local <65554107+sarmstead@users.noreply.github.com>
2021-06-13 20:37:30 -0600

    Create default props object.

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,4 +2,9 @@ const Button = ({ color, text }) => {
     return <button style={{ backgroundColor: color }} className='btn'>{text}</button>
 }
 
+Button.defaultProps = {
+    color: 'steelblue',
+    text: 'Add'
+}
+
 export default Button;
