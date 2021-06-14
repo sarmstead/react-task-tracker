802fd6793be5663cf919eac14f55efcd64211f1c
sunjay_local <65554107+sarmstead@users.noreply.github.com>
2021-06-13 20:40:10 -0600

    Import and add propTypes object..

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types';
+
 const Button = ({ color, text }) => {
     return <button style={{ backgroundColor: color }} className='btn'>{text}</button>
 }
@@ -5,6 +7,11 @@ const Button = ({ color, text }) => {
 Button.defaultProps = {
     color: 'steelblue',
     text: 'Add'
+}
+
+Button.propTypes = {
+    text: PropTypes.string,
+    color: PropTypes.string
 }
 
 export default Button;
