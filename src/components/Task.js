55f7db4fd9329e303162a29af1af29e2ffb58f95
sunjay_local <65554107+sarmstead@users.noreply.github.com>
2021-06-14 19:08:18 -0600

    Pass in task prop. Return basic JSX structure.

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,5 +1,9 @@
-const Task = () => {
-
+const Task = ({ task }) => {
+    return (
+        <div className='task'>
+            <h3>{task.text}</h3>
+        </div>
+    )
 }
 
 export default Task;
