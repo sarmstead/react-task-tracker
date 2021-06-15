248f5d78de5ab857e603ca7a62c3c1f2ac026916
sunjay_local <65554107+sarmstead@users.noreply.github.com>
2021-06-14 19:29:07 -0600

    Create deleteTask function to delete task items, and add it to onDelete prop in Tasks

--- src/App.js
+++ src/App.js
@@ -23,10 +23,17 @@ function App() {
         reminder: true
     },
 ])
+  // Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => 
+    task.id !== id
+    ))
+  }
+
   return (
     <section className="container">
       <Header />
-      <Tasks tasks={tasks} />
+      <Tasks tasks={tasks} onDelete={deleteTask} />
     </section>
   );
 }
