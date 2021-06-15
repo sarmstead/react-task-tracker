3a4bf4b36c07ff4070916b4d84f64c463f1e3cf4
sunjay_local <65554107+sarmstead@users.noreply.github.com>
2021-06-14 18:58:02 -0600

    Remove tasks state. Pass in destructured task prop

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,26 +1,6 @@
-import { useState } from 'react';
 
-const Tasks = () => {
-    const [tasks, setTasks] = useState([
-        {
-            id: 1,
-            text: 'Doctor Appointment',
-            day: 'July 1 at 1 PM',
-            reminder: true
-        },
-        {
-            id: 2,
-            text: 'Dentist Appointment',
-            day: 'July 2 at 1 PM',
-            reminder: true
-        },
-        {
-            id: 3,
-            text: 'Mechanic Appointment',
-            day: 'July 3 at 1 PM',
-            reminder: true
-        },
-    ])
+const Tasks = ({ tasks }) => {
+
     return (
         <>
             {tasks.map((task) => (
