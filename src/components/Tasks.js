40b7abdc2607fdaabd982ad6b40a6e9bfc126bba
sunjay_local <65554107+sarmstead@users.noreply.github.com>
2021-06-14 18:49:45 -0600

    Import useState and create task state. Map through tasks in return.

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,7 +1,34 @@
-
+import { useState } from 'react';
 
 const Tasks = () => {
-
+    const [tasks, setTasks] = useState([
+        {
+            id: 1,
+            text: 'Doctor Appointment',
+            day: 'July 1 at 1 PM',
+            reminder: true
+        },
+        {
+            id: 2,
+            text: 'Dentist Appointment',
+            day: 'July 2 at 1 PM',
+            reminder: true
+        },
+        {
+            id: 3,
+            text: 'Mechanic Appointment',
+            day: 'July 3 at 1 PM',
+            reminder: true
+        },
+    ])
+    return (
+        <>
+            {tasks.map((task) => (
+                <h3 key={task.id}>{task.text}</h3>
+            )
+            )}
+        </>
+    )
 }
 
 export default Tasks;
